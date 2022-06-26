90ef1be0eca47ca06a1acdce56e9765a4e8dda35
lrchema <lrchema@gmail.com>
2022-06-26 18:31:06 +0800

    Setup gsutil and save steps

---
+++ bin/hist.sh
@@ -0,0 +1,20 @@
+~/google-cloud-sdk/bin/gcloud init
+~/google-cloud-sdk/bin/gcloud components list
+~/google-cloud-sdk/bin/gcloud projects create fourthtry-4f93f
+~/google-cloud-sdk/bin/gcloud projects
+~/google-cloud-sdk/bin/gcloud projects list
+~/google-cloud-sdk/bin/gsutil mb -b on -l us-central1 gs://fourthtry-4f93f/
+~/google-cloud-sdk/bin/gsutil cp /Users/hema/Desktop/Personal/Art/20190414_202054.jpg  gs://fourthtry-4f93f/
+~/google-cloud-sdk/bin/gsutil ls gs://fourthtry-4f93f/
+cd /Users/hema/Desktop/Personal/SmolBusinessLol/SWE/RagaSurabhi/fourthTry 
+ls
+~/google-cloud-sdk/bin/gsutil cp gs://fourthtry-4f93f/20190414_202054.jpg gs://fourthtry-4f93f/drawings/20190414_202054.jpg
+~/google-cloud-sdk/bin/gsutil ls gs://fourthtry-4f93f/
+~/google-cloud-sdk/bin/gsutil ls //fourthtry-4f93f/drawings
+~/google-cloud-sdk/bin/gsutil ls gs://fourthtry-4f93f/drawings
+~/google-cloud-sdk/bin/gsutil rm gs://fourthtry-4f93f/20190414_202054.jpg 
+~/google-cloud-sdk/bin/gsutil ls gs://fourthtry-4f93f/
+~/google-cloud-sdk/bin/gsutil ls 
+~/google-cloud-sdk/bin/gsutil ls gs://fourthtry-4f93f.appspot.com/
+~/google-cloud-sdk/bin/gsutil cp /Users/hema/Desktop/Personal/Art/20190414_202054.jpg gs://fourthtry-4f93f.appspot.com/art/
+
